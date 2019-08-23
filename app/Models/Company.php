eb81ec0ef215896145aca9cbbad33c40beacd9ca
Olamide <olakehinde59@gmail.com>
2019-08-23 17:20:14 +0100

    added fillables for comoany model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    //
+	use SoftDeletes;
+
+    protected $fillable = [
+    	'name',
+    	'address',
+    	'email',
+    	'sector'
+    ];
+
+    /**
+    * This company hasMany Employees
+    */
+    public function employees () {
+    	return $this->hasMany('App\Models\Employee');
+    }
 }
