c8313405201d25091324b6f5e0928c0911be6c6d
Olamide <olakehinde59@gmail.com>
2019-08-23 16:44:22 +0100

    fixed typo error in the migration file

--- database/migrations/2019_08_23_145220_create_companies_table.php
+++ database/migrations/2019_08_23_145220_create_companies_table.php
@@ -17,7 +17,7 @@ class CreateCompaniesTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('address');
-            $table->string('email')-unique();
+            $table->string('email')->unique();
             $table->string('sector')->nullable();
             $table->softDeletes();
             $table->timestamps();
