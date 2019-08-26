b4fce082ba215f299b39c7f1497579baf6b2e010
Olamide <olakehinde59@gmail.com>
2019-08-26 18:07:17 +0100

    refactored the create view. removed the form to a separate file to make it reusable

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -26,25 +26,10 @@
 						</ul>
 					</div><br />
 			    @endif
+				
 				<form method="post" action="{{ route('companies.store') }}">
-					<div class="form-group">
-					  @csrf
-					  <label for="name">Company Name:</label>
-					  <input type="text" class="form-control" name="name"/>
-					</div>
-				  	<div class="form-group">
-				      	<label for="price">Address:</label>
-				      	<input type="text" class="form-control" name="address"/>
-				  	</div>
-				  	<div class="form-group">
-				      	<label for="quantity">Email:</label>
-				      	<input type="text" class="form-control" name="email"/>
-				  	</div>
-				  	<div class="form-group">
-				      	<label for="quantity">Sector <small>(optional)</small>:</label>
-				      	<input type="text" class="form-control" name="sector"/>
-				  	</div>
-				  	<button type="submit" class="btn btn-primary"><i class="fa fa-check"></i> Create Company</button>
+					@csrf
+					@include('companies.fields')
 				</form>
 		  	</div>
 		</div>
