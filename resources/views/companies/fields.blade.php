a2e787bea4fa914ba0594f73843169c133768aad
Olamide <olakehinde59@gmail.com>
2019-08-26 18:10:15 +0100

    created a reusable form fields page for companies

---
+++ resources/views/companies/fields.blade.php
@@ -0,0 +1,17 @@
+<div class="form-group">
+  	<label for="name">Company Name:</label>
+  	<input type="text" required="" class="form-control" name="name"/>
+</div>
+<div class="form-group">
+	<label for="price">Address:</label>
+	<input type="text" required="" class="form-control" name="address"/>
+</div>
+<div class="form-group">
+	<label for="quantity">Email:</label>
+	<input type="text" required="" class="form-control" name="email"/>
+</div>
+<div class="form-group">
+	<label for="quantity">Sector <small>(optional)</small>:</label>
+	<input type="text" class="form-control" name="sector"/>
+</div>
+<button type="submit" class="btn btn-primary"><i class="fa fa-check"></i> Create Company</button>
