51d15e1a2c695ec9edcf476038e76a2077713c64
Olamide <olakehinde59@gmail.com>
2019-08-27 15:22:59 +0100

    corrected mismatch or misplacement of the variable names in the value attribute fields

--- resources/views/companies/fields.blade.php
+++ resources/views/companies/fields.blade.php
@@ -4,14 +4,15 @@
 </div>
 <div class="form-group">
 	<label for="price">Address:</label>
-	<input type="text" required="" class="form-control" name="address" value="{{$company->name}}"/>
+	<input type="text" required="" class="form-control" name="address" value="{{$company->address}}"/>
 </div>
 <div class="form-group">
 	<label for="quantity">Email:</label>
-	<input type="text" required="" class="form-control" name="email" value="{{$company->name}}"/>
+	<input type="text" required="" class="form-control" name="email" value="{{$company->email}}"/>
 </div>
 <div class="form-group">
 	<label for="quantity">Sector <small>(optional)</small>:</label>
 	<input type="text" class="form-control" name="sector"  value="{{$company->sector}}"/>
 </div>
+
 
