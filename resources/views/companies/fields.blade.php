82aa30f3ac1c7e9379a63a16544f0a8f791553e5
Olamide <olakehinde59@gmail.com>
2019-08-27 17:47:44 +0100

    fixed 'undefined variable' bug

--- resources/views/companies/fields.blade.php
+++ resources/views/companies/fields.blade.php
@@ -1,18 +1,18 @@
 <div class="form-group">
   	<label for="name">Company Name:</label>
-  	<input type="text" required="" class="form-control" name="name" value="{{$company->name}}"/>
+  	<input type="text" required="" class="form-control" name="name" value="{{ isset($company->name) ? $company->name : null}}"/>
 </div>
 <div class="form-group">
 	<label for="price">Address:</label>
-	<input type="text" required="" class="form-control" name="address" value="{{$company->address}}"/>
+	<input type="text" required="" class="form-control" name="address" value="{{isset($company->address) ? $company->address : null}}"/>
 </div>
 <div class="form-group">
 	<label for="quantity">Email:</label>
-	<input type="text" required="" class="form-control" name="email" value="{{$company->email}}"/>
+	<input type="text" required="" class="form-control" name="email" value="{{isset($company->email) ? $company->email : null }}"/>
 </div>
 <div class="form-group">
 	<label for="quantity">Sector <small>(optional)</small>:</label>
-	<input type="text" class="form-control" name="sector"  value="{{$company->sector}}"/>
+	<input type="text" class="form-control" name="sector"  value="{{isset($company->sector) ? $company->sector : null}}"/>
 </div>
 
 
