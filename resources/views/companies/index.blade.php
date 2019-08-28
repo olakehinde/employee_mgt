c9c1aeec06ec0125c2f395959b63e1f86e8c4abc
Olamide <olakehinde59@gmail.com>
2019-08-28 14:28:40 +0100

    fixed the 'undefined variable' bug. removed the count()

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -13,16 +13,20 @@
 	            </div>
 	        @endif
 			<div class="col-sm-6 col-md-10 pull-left">
-				<h3>Company Records</h3>
+				<h2>Company Records</h2>
 			</div>
 			<div class="pull-right">
-				<a href="#" class="btn btn-sm btn-primary">New Company</a>
+				<a href="{{ route('companies.create') }}" class="btn btn-sm btn-primary"><i class="fa fa-plus"></i> New Company</a>
 			</div>
 		</div>
+		@if(!isset($companies))
+			<div class="px-3 mx-3 py-3 card">
+				<h5>No Company created yet.</h5>
+			</div>
+		@else
 		<table class="table">
 			<thead class="thead-dark">
 				<tr>
-					<th scope="col">#</th>
 					<th scope="col">Company</th>
 					<th scope="col">Address</th>
 					<th scope="col">Email</th>
@@ -33,26 +37,33 @@
 		  	<tbody>
 		  		@foreach($companies as $company)
 		  		<tr>
-			      <th scope="row">{{ $company->id }}</th>
 			      <td>{{ $company->name }}</td>
 					<td>{{ $company->address }}</td>
 					<td>{{ $company->email }}</td>
 					<td>{{ is_null($company->sector) ? 'Not available' : $company->sector }}</td>
 					<td>
-						<a href="{{ route('companies.show', $company->id)}}"><span class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> </span>
-						</a>
-						<a href="{{ route('companies.edit', $company->id)}}"><span class="btn btn-sm btn-success"><i class="fa fa-edit"></i> </span>
-						</a>
-
-						<form action="{{ route('companies.destroy', $company->id)}}" method="post">
-		                  	@csrf
-		                  	@method('DELETE')
-		                  	<button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash"></i> </button>
-		                </form>
+						<div class="row">
+							<div>
+								<a href="{{ route('companies.show', $company->id)}}"><span class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> View</span>
+								</a>
+							</div>
+							<div class="px-1">
+								<a href="{{ route('companies.edit', $company->id)}}"><span class="btn btn-sm btn-success"><i class="fa fa-edit"></i> Edit</span>
+								</a>
+							</div>
+							<div>
+								<form action="{{ route('companies.destroy', $company->id)}}" method="post">
+				                  	@csrf
+				                  	@method('DELETE')
+				                  	<button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash"></i> Delete</button>
+				                </form>
+							</div>
+						</div>
 					</td>
 			    </tr>
 			    @endforeach
 		  </tbody>
 		</table>
+		@endif
 	</div>
 @endsection
