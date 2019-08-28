a6c724b594a0600e2cba33da9f8bd16f5755e4aa
Olamide <olakehinde59@gmail.com>
2019-08-28 16:20:22 +0100

    created a 'many-to-one' relationship btw employees and company

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+    	'first_name',
+    	'last_name',
+    	'gender',
+    	'email',
+    	'company_id'
+    ];
+
+    /**
+    * This Employee belongs to a company
+    */
+    public function company () {
+    	return $this->belongsTo('App\Models\Company');
+    }
 }
