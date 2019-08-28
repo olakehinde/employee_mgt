c08bd9b5f59d26181c258ad4efd4239813bc2335
Olamide <olakehinde59@gmail.com>
2019-08-28 16:18:59 +0100

    created the empolyee index method to display the index page

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -14,7 +14,8 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        //
+        $employees = Employee::all();
+        return view('employees.index', compact($employees));
     }
 
     /**
