cc35de755914c55ffd4a1072f50453f2fac04440
Olamide <olakehinde59@gmail.com>
2019-08-28 16:49:57 +0100

    created the form fields for creating employee

--- resources/views/employees/fields.blade.php
+++ resources/views/employees/fields.blade.php
@@ -1,18 +1,33 @@
 <div class="form-group">
-  	<label for="name">Company Name:</label>
-  	<input type="text" required="" class="form-control" name="name" value="{{ isset($company->name) ? $company->name : null}}"/>
+  	<label for="name">First Name:</label>
+  	<input type="text" required="" class="form-control" name="f_name" value="{{ isset($company->name) ? $company->name : null}}"/>
 </div>
 <div class="form-group">
-	<label for="price">Address:</label>
-	<input type="text" required="" class="form-control" name="address" value="{{isset($company->address) ? $company->address : null}}"/>
+  	<label for="name">Last Name:</label>
+  	<input type="text" required="" class="form-control" name="l_name" value="{{ isset($company->name) ? $company->name : null}}"/>
+</div>
+<div class="form-group row pl-3">
+	<label for="gender">Gender</label>
+	<div class="custom-control custom-radio px-5">
+	    <input type="radio" class="custom-control-input" id="gender" name="gender" value="Male">
+	    <label class="custom-control-label" for="gender">Male</label>
+	</div>
+
+	<div class="custom-control custom-radio">
+	    <input type="radio" class="custom-control-input" id="gender" name="gender" value="Female">
+	    <label class="custom-control-label" for="gender">Female</label>
+	</div>
 </div>
 <div class="form-group">
 	<label for="quantity">Email:</label>
 	<input type="text" required="" class="form-control" name="email" value="{{isset($company->email) ? $company->email : null }}"/>
 </div>
 <div class="form-group">
-	<label for="quantity">Sector <small>(optional)</small>:</label>
-	<input type="text" class="form-control" name="sector"  value="{{isset($company->sector) ? $company->sector : null}}"/>
+	<label for="company">Company</label>
+	<select class="form-control" name="company">
+		<option class="" selected="selected" disabled="disabled">Select Company</option>
+		@foreach($companies as $company)
+			<option value="{{$company->id}}">{{$company->name}}</option>
+		@endforeach
+	</select>
 </div>
-
-
