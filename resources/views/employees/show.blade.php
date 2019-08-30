f5fe7d7df19051c85521d53e45b22f6cc400ed3c
Olamide <olakehinde59@gmail.com>
2019-08-30 09:18:51 +0100

    display employee details

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -4,12 +4,11 @@
 	<div class="col-sm-6 col-md-d offset-md-3">
 		<div class="card">
 			<div class="card-header">
-				<h3>{{ $company->name }}</h3>
+				<h3>{{ $employee->first_name . ' '. $employee->last_name }} <span class="small"> - {{ $employee->company['name'] }} </span></h3>
 			</div>
 			<div class="card-body">
-				<div><b>Address:</b> {{ $company->address }}</div>
-				<div><b>Email:</b> {{ $company->email }}</div>
-				<div><b>Sector:</b> {{ $company->sector ? $company->sector : 'Not Available' }}</div>
+				<div><b>Email:</b> {{ $employee->email }}</div>
+				<div><b>Gender:</b> {{ $employee->gender }}</div>
 			</div>
 		</div>
 	</div>
